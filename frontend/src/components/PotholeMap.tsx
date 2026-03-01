2966e42472f7f5fee71de5b32f872a2bf898b5ec
aorlov05 <aorlov@udel.edu>
2026-03-01 08:36:41 -0500

    Fix leaflet icons on deploy

--- frontend/src/components/PotholeMap.tsx
+++ frontend/src/components/PotholeMap.tsx
@@ -5,6 +5,19 @@ import { useNavigate } from '@tanstack/react-router'
 import { supabase } from '../supabaseClient'
 import 'leaflet/dist/leaflet.css'
 
+import L from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
 export interface Pothole {
   id: string;
   image_url: string;
